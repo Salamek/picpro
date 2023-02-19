6728a725fb65a8c77b201d687e4a99efcb6cf200
Adam Schubert <adam.schubert@sg1-game.net>
2023-02-19 19:07:43 +0100

    New version 0.0.9

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_readme() -> str:
 
 setup(
     name='picpro',
-    version='0.0.8',
+    version='0.0.9',
     packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={'picpro': ['py.typed']},
     install_requires=[
