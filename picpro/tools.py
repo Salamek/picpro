5a7df866c3f758f6a949896c56eb08d4a2d8e324
Adam Schubert <adam.schubert@sg1-game.net>
2025-04-07 16:01:12 +0200

    feat(Tools): Remove unused functions

--- picpro/tools.py
+++ picpro/tools.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 
 def indexwise_and(fuses: list, setting_values: list) -> list:
     """Given a list of fuse values, and a list of (index, value) pairs,
@@ -20,62 +18,3 @@ def swab_bytes(to_swab: bytes) -> bytes:
     return bytes(result)
 
 
-def swab_record(record: Tuple[int, bytes]) -> Tuple[int, bytes]:
-    """Given a record from a hex file, return a new copy with adjacent data bytes swapped."""
-    return record[0], swab_bytes(record[1])
-
-
-def range_filter_records(records: list, lower_bound: int, upper_bound: int) -> list:
-    """Given a list of HEX file records, return a new list of HEX file records containing only the HEX data within the specified address range."""
-    result = []
-
-    for record in records:
-        # Need to handle five cases:
-        # 1: record is completely below lower bound - do nothing
-        # 2: record is partially below lower bound - slice and append
-        # 3: record is in range - append
-        # 4: record is partially above upper bound - slice and append
-        # 5: record is completely above upper bound - do nothing
-        if ((record[0] >= lower_bound) and
-                (record[0] < upper_bound)):
-            # lower bound is in range and therefore needn't change.
-            # Part or all of this record will appear in output.
-            if (record[0] + len(record[1])) < upper_bound:
-                # case 3
-                result.append(record)
-            else:
-                # case 4
-                slice_length = upper_bound - record[0]
-                result.append((record[0], record[1][0:slice_length]))
-        elif record[0] < lower_bound < (record[0] + len(record[1])):
-            # case 2
-            slice_pos = lower_bound - record[0]
-            result.append((lower_bound, record[1][slice_pos:len(record[1])]))
-    return result
-
-
-def merge_records(records: list, default_data: bytes, base_address: int = 0) -> bytes:
-    """Given a list of HEX file records and a data buffer with its own base address (default=0), merge the HEX file records into a new copy of the data buffer."""
-    result_list = bytearray()
-    mark = 0
-    for record in records:
-        if record[0] < base_address:
-            raise IndexError('Record address out of range.')
-
-        if (record[0] + len(record[1])) > (base_address + len(default_data)):
-            raise IndexError('Record out of range.')
-
-        point = record[0] - base_address
-        if mark != point:
-            result_list += default_data[mark:point]
-            mark = point
-        # Now we can add the record data to result_list.
-        result_list += record[1]
-        mark += len(record[1])
-    # Fill out the rest of the result with data from default_data, if
-    # necessary.
-    if mark < len(default_data):
-        result_list += default_data[mark:]
-
-    # String-join result_list and return.
-    return bytes(result_list)
